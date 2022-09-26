b12f20258a383aada990b71d2802ae66f503dc33
Marble <62856659+Marble5@users.noreply.github.com>
2022-09-26 15:58:45 +0200

    Nyt Dokument / Method Reference

--- src/Del1.java
+++ src/Del1.java
@@ -1,22 +1,25 @@
-
 import java.util.Scanner;
 public class Del1 {
     public static void main (String [] args){
 
-boolean playing =true;
+        //Method reference
+        
 
-boolean player2 = false;
+        //Boolean værdier
+        boolean playing =true;
+        boolean player2 = false;
 
         Scanner scanner = new Scanner(System.in);
 
         while(playing) {
 
+            //player 1
             System.out.println("Player 1 spiller nu");
-
             System.out.println("Tryk enter for at spille");
             String p1 = scanner.nextLine();
-            System.out.println("Player 2 spiller nu");
 
+            //player 2
+            System.out.println("Player 2 spiller nu");
             System.out.println("Tryk enter for at spille");
             String p2 = scanner.nextLine();
 
---
+++ src/Terninger.java
@@ -0,0 +1,6 @@
+//Class for terninger
+public class Terninger {
+    public void rafelBæger(){
+        System.out.println("rafelBæger");
+    }
+}
