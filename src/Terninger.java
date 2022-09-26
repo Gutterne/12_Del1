f149611b01b3c0d1c387f147c0710f6f0e262c34
mathias124 <mfs2001@hotmail.dk>
2022-09-26 17:00:20 +0200

    Kommentarer.

--- src/Terninger.java
+++ src/Terninger.java
@@ -4,8 +4,7 @@ import java.util.Random;
 public class Terninger {
     int terningSum;
     int sum;
-    Terninger terninger;
-
+    //Her laves en konstrukter og gør det object.
     Terninger(int sum) {
 this.sum = terningSum;
     }
