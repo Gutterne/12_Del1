497053804e4fbcdf1a7cba8a6acab568af5f59d5
mathias124 <mfs2001@hotmail.dk>
2022-09-26 15:50:26 +0200

    Scanner for begge indlæst

--- src/Del1.java
+++ src/Del1.java
@@ -4,12 +4,22 @@ public class Del1 {
     public static void main (String [] args){
 
 boolean playing =true;
+
+boolean player2 = false;
+
         Scanner scanner = new Scanner(System.in);
 
         while(playing) {
 
+            System.out.println("Player 1 spiller nu");
+
             System.out.println("Tryk enter for at spille");
-            String inp = scanner.nextLine();
+            String p1 = scanner.nextLine();
+            System.out.println("Player 2 spiller nu");
+
+            System.out.println("Tryk enter for at spille");
+            String p2 = scanner.nextLine();
+
 
 
         }
