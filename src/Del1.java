fada1f6eb856ce3da671174e1930678c41ccb2be
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2022-09-27 17:24:47 +0200

    FIk det til virke, men var afvist

--- src/Del1.java
+++ src/Del1.java
@@ -7,13 +7,14 @@ public class Del1 {
 
        // Terninger terningObject = new Terninger(terningSum);
 
-        Terninger terninger = new Terninger(terningSum);
+
 
         //Boolean værdier
         boolean playing =true;
         boolean player2 = false;
 
         Scanner scanner = new Scanner(System.in);
+        int points1=0, points2=0;
 
         while(playing) {
 
@@ -21,9 +22,13 @@ public class Del1 {
             System.out.println("Player 1 spiller nu");
             System.out.println("Tryk enter for at spille");
             String p1 = scanner.nextLine();
-            System.out.print("Player 1, har kastet: ");
-            terninger.rafelBaeger(terningSum);
-            System.out.println("");
+
+            int terning1 = (int) (Math.random()*6+1);
+            int terning2 = (int) (Math.random()*6+1);
+            System.out.print("Player 1, har kastet:"+ terning1+" og "+terning2);
+            int sum = terning1+terning2;
+            points1+= sum;
+            System.out.println("  " + points1);
 
 
             //player 2
@@ -32,8 +37,13 @@ public class Del1 {
             String p2 = scanner.nextLine();
 
             System.out.print("Player 2, har kastet: ");
-            terninger.rafelBaeger(terningSum);
-            System.out.println("");
+            int terning3 = (int) (Math.random()*6+1);
+            int terning4 = (int) (Math.random()*6+1);
+            System.out.print("Player 2, har kastet: "+terning3+" "+terning4);
+            int num = terning3+terning4;
+            points2+= num;
+
+            System.out.println(" "+points2);
         }
 }
 
