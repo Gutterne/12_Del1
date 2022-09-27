ebea3b7b2dd3ff80aeac15b46218e07ae8e9d7f1
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2022-09-27 17:24:47 +0200

    FIk det til virke, men var afvist

--- src/Terninger.java
+++ src/Terninger.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 //Class for terninger
 public class Terninger {
     int terningSum;
@@ -9,15 +7,16 @@ public class Terninger {
 this.sum = terningSum;
     }
 
-    public void rafelBaeger(int terningSum) {
+    public int rafelBaeger(int terningSum) {
 
             sum = terningSum;
             int size = 6; //Størrelsen af vores random interval, Indeholder 0-5, hermed 6 cifre, hvori der består 0.
 
-            int terning1 = (int) (Math.random()*size+1); //(+1) afrykker vores interval et tal over, så det er fra 1-6
-            int terning2 = (int) (Math.random()*size+1);
+            int terning1 = (int) (Math.random()*6+1); //(+1) afrykker vores interval et tal over, så det er fra 1-6
+            int terning2 = (int) (Math.random()*6+1);
             sum= terning1 + terning2;
             System.out.println(sum);
-        }
+        return size;
+    }
     }
 
