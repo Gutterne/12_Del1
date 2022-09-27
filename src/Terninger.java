e08b946747451890d4b3523b09b4ee8f22aaa0fa
mathias124 <mfs2001@hotmail.dk>
2022-09-27 18:19:17 +0200

    ResetCheck - 2 ene's og du bliver doomed(score reset).
    Version 1.1.0

--- src/Terninger.java
+++ src/Terninger.java
@@ -4,7 +4,7 @@ public class Terninger {
     int sum;
     int temp1;
     int temp2;
-
+ boolean doomed =false;
     int temp1playersum =0;
     int temp2playersum = 0;
     //Her laves en konstrukter og gør det object.
@@ -14,6 +14,7 @@ public class Terninger {
     this.temp2 = temp2playersum;
     }
     public void rafelBaeger(int terningSum) {
+
             //temp1 = temp1playersum;
             sum = terningSum;
             int size = 6; //Størrelsen af vores random interval, Indeholder 0-5, hermed 6 cifre, hvori der består 0.
@@ -21,6 +22,13 @@ public class Terninger {
             int terning1 = (int) (Math.random()*size+1); //(+1) afrykker vores interval et tal over, så det er fra 1-6
             int terning2 = (int) (Math.random()*size+1);
             sum= terning1 + terning2;
+            if(sum ==2) {
+                doomed = true;
+            } else {
+                doomed =false;
+            }
+
+
 
         //temp1playersum = temp1 + terningSum;
 
@@ -31,6 +39,9 @@ public class Terninger {
         }
         public void player1 (int temp1playersum){
         temp1 += temp1playersum + sum;
+            if(doomed) {
+            temp1 =0;
+            }
             System.out.println();
             System.out.println(sum+" Du har slået");
             System.out.println(temp1+ " Du har nuværende");
@@ -41,16 +52,18 @@ public class Terninger {
             }
             }
     public void player2 (int temp2playersum){
-
-        temp2 += temp2playersum + sum;
-        System.out.println();
-        System.out.println(sum+"  Du har slået");
-        System.out.println(temp2 + "  Du har nuværende");
-        System.out.println();
-        if(temp2 >= 40) {
-            System.out.println("VI HAR EN VINDER DING DING DING, player 2 Woop Woop!");
-            System.exit(terningSum);
+        if(doomed) {
+            temp2 = 0;
         }
+            temp2 += temp2playersum + sum;
+            System.out.println();
+            System.out.println(sum + "  Du har slået");
+            System.out.println(temp2 + "  Du har nuværende");
+            System.out.println();
+            if (temp2 >= 40) {
+                System.out.println("VI HAR EN VINDER DING DING DING, player 2 Woop Woop!");
+                System.exit(terningSum);
+            }
 
     }
     }
