5f48ae8ee54629275e383a307ccbcb17f1cb9dd7
Casper Blichfeldt <casper.h.blichfeldt@live.dk>
2022-09-28 11:03:23 +0200

    feature 1.2 release. double roll

--- src/Terninger.java
+++ src/Terninger.java
@@ -1,17 +1,24 @@
 //Class for terninger
 public class Terninger {
     int terningSum;
+    int terningSum2;
     int sum;
+    int sum2;
     int temp1;
     int temp2;
+
  boolean doomed =false;
+ boolean ekstra =false;
     int temp1playersum =0;
     int temp2playersum = 0;
     //Her laves en konstrukter og gør det object.
     Terninger(int sum) {
     this.sum = terningSum;
+    this.sum2=terningSum2;
     this.temp1 = temp1playersum;
     this.temp2 = temp2playersum;
+
+
     }
     public void rafelBaeger(int terningSum) {
 
@@ -27,7 +34,18 @@ public class Terninger {
             } else {
                 doomed =false;
             }
-
+            // Nyt sæt terninger til ekstra slag
+            if (terning1==terning2){
+                sum2 = terningSum2;
+                int size2 = 6;
+                ekstra=true;
+                int terning3 = (int) (Math.random()*size2+1); //(+1) afrykker vores interval et tal over, så det er fra 1-6
+                int terning4 = (int) (Math.random()*size2+1);
+                sum2 =terning3+terning4;
+            }
+            else{
+                ekstra=false;
+            }
 
 
         //temp1playersum = temp1 + terningSum;
@@ -46,6 +64,17 @@ public class Terninger {
             System.out.println("Du har slået: "+sum);
             System.out.println("Nuværende Points: "+temp1);
             System.out.println();
+            //Ekstra slag
+            if(ekstra){
+                if(doomed){
+                    temp1=0;
+                }
+                temp1 += temp1playersum + sum2;
+                System.out.println();
+                System.out.println("Du har slået: "+sum2);
+                System.out.println("Nuværende Points: "+temp1 );
+                System.out.println();
+            }
             if(temp1 >= 40) {
                 System.out.println("VI HAR EN VINDER DING DING DING Player 1 yeha!");
                 System.exit(terningSum);
@@ -55,11 +84,25 @@ public class Terninger {
         if(doomed) {
             temp2 = 0;
         }
+
             temp2 += temp2playersum + sum;
             System.out.println();
             System.out.println("Du har slået: "+sum);
             System.out.println("Nuværende Points: "+temp2);
             System.out.println();
+            //Ekstra slag
+        if(ekstra){
+            if(doomed){
+                temp2=0;
+            }
+            temp2 += temp2playersum + sum2;
+            System.out.println();
+            System.out.println("Du har slået: "+sum2);
+            System.out.println("Nuværende Points: "+temp2);
+            System.out.println();
+        }
+
+
             if (temp2 >= 40) {
                 System.out.println("VI HAR EN VINDER DING DING DING, player 2 Woop Woop!");
                 System.exit(terningSum);
